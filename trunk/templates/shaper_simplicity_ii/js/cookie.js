0c4f3d9892455fe3510febd92a710191280f537e
rpatel@digite.com <rpatel@digite.com@9b834a4c-f555-3c93-554b-b0e9f3194743>
2013-12-17 04:07:17 +0000

    updating latest 1.4 cookie plugin

--- trunk/templates/shaper_simplicity_ii/js/cookie.js
+++ trunk/templates/shaper_simplicity_ii/js/cookie.js
@@ -1,43 +1,68 @@
 /*!
- * jQuery Cookie Plugin v1.3
+ * jQuery Cookie Plugin v1.4.0
  * https://github.com/carhartl/jquery-cookie
  *
- * Copyright 2011, Klaus Hartl
- * Dual licensed under the MIT or GPL Version 2 licenses.
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.opensource.org/licenses/GPL-2.0
+ * Copyright 2013 Klaus Hartl
+ * Released under the MIT license
  */
-(function ($, document, undefined) {
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as anonymous module.
+        define(['jquery'], factory);
+    } else {
+        // Browser globals.
+        factory(jQuery);
+    }
+}(function ($) {
 
     var pluses = /\+/g;
 
-    function raw(s) {
-        return s;
+    function encode(s) {
+        return config.raw ? s : encodeURIComponent(s);
     }
 
-    function decoded(s) {
-        return decodeURIComponent(s.replace(pluses, ' '));
+    function decode(s) {
+        return config.raw ? s : decodeURIComponent(s);
+    }
+
+    function stringifyCookieValue(value) {
+        return encode(config.json ? JSON.stringify(value) : String(value));
+    }
+
+    function parseCookieValue(s) {
+        if (s.indexOf('"') === 0) {
+            // This is a quoted cookie as according to RFC2068, unescape...
+            s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+        }
+
+        try {
+            // Replace server-side written pluses with spaces.
+            // If we can't decode the cookie, ignore it, it's unusable.
+            // If we can't parse the cookie, ignore it, it's unusable.
+            s = decodeURIComponent(s.replace(pluses, ' '));
+            return config.json ? JSON.parse(s) : s;
+        } catch(e) {}
+    }
+
+    function read(s, converter) {
+        var value = config.raw ? s : parseCookieValue(s);
+        return $.isFunction(converter) ? converter(value) : value;
     }
 
     var config = $.cookie = function (key, value, options) {
 
-        // write
-        if (value !== undefined) {
-            options = $.extend({}, config.defaults, options);
+        // Write
 
-            if (value === null) {
-                options.expires = -1;
-            }
+        if (value !== undefined && !$.isFunction(value)) {
+            options = $.extend({}, config.defaults, options);
 
             if (typeof options.expires === 'number') {
                 var days = options.expires, t = options.expires = new Date();
-                t.setDate(t.getDate() + days);
+                t.setTime(+t + days * 864e+5);
             }
 
-            value = config.json ? JSON.stringify(value) : String(value);
-
             return (document.cookie = [
-                encodeURIComponent(key), '=', config.raw ? value : encodeURIComponent(value),
+                encode(key), '=', stringifyCookieValue(value),
                 options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
                 options.path    ? '; path=' + options.path : '',
                 options.domain  ? '; domain=' + options.domain : '',
@@ -45,37 +70,45 @@
             ].join(''));
         }
 
-        // read
-        var decode = config.raw ? raw : decoded;
-        var cookies = document.cookie.split('; ');
-        for (var i = 0, parts; (parts = cookies[i] && cookies[i].split('=')); i++) {
-            if (decode(parts.shift()) === key) {
-                var cookie = decode(parts.join('='));
-                return config.json ? JSON.parse(cookie) : cookie;
+        // Read
+
+        var result = key ? undefined : {};
+
+        // To prevent the for loop in the first place assign an empty array
+        // in case there are no cookies at all. Also prevents odd result when
+        // calling $.cookie().
+        var cookies = document.cookie ? document.cookie.split('; ') : [];
+
+        for (var i = 0, l = cookies.length; i < l; i++) {
+            var parts = cookies[i].split('=');
+            var name = decode(parts.shift());
+            var cookie = parts.join('=');
+
+            if (key && key === name) {
+                // If second argument (value) is a function it's a converter...
+                result = read(cookie, value);
+                break;
+            }
+
+            // Prevent storing a cookie that we couldn't decode.
+            if (!key && (cookie = read(cookie)) !== undefined) {
+                result[name] = cookie;
             }
         }
 
-        return null;
+        return result;
     };
 
     config.defaults = {};
 
     $.removeCookie = function (key, options) {
-        if ($.cookie(key) !== null) {
-            $.cookie(key, null, options);
-            return true;
+        if ($.cookie(key) === undefined) {
+            return false;
         }
-        return false;
+
+        // Must not alter options, thus extending a fresh object...
+        $.cookie(key, '', $.extend({}, options, { expires: -1 }));
+        return !$.cookie(key);
     };
 
-})(jQuery, document);
-
-/*
- * jQuery doTimeout: Like setTimeout, but better! - v1.0 - 3/3/2010
- * http://benalman.com/projects/jquery-dotimeout-plugin/
- *
- * Copyright (c) 2010 "Cowboy" Ben Alman
- * Dual licensed under the MIT and GPL licenses.
- * http://benalman.com/about/license/
- */
-(function($){var a={},c="doTimeout",d=Array.prototype.slice;$[c]=function(){return b.apply(window,[0].concat(d.call(arguments)))};$.fn[c]=function(){var f=d.call(arguments),e=b.apply(this,[c+f[0]].concat(f));return typeof f[0]==="number"||typeof f[1]==="number"?this:e};function b(l){var m=this,h,k={},g=l?$.fn:$,n=arguments,i=4,f=n[1],j=n[2],p=n[3];if(typeof f!=="string"){i--;f=l=0;j=n[1];p=n[2]}if(l){h=m.eq(0);h.data(l,k=h.data(l)||{})}else{if(f){k=a[f]||(a[f]={})}}k.id&&clearTimeout(k.id);delete k.id;function e(){if(l){h.removeData(l)}else{if(f){delete a[f]}}}function o(){k.id=setTimeout(function(){k.fn()},j)}if(p){k.fn=function(q){if(typeof p==="string"){p=g[p]}p.apply(m,d.call(n,i))===true&&!q?o():e()};o()}else{if(k.fn){j===undefined?e():k.fn(j===false);return true}else{e()}}}})(jQuery);
+}));
