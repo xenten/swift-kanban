a962bba6e700da13c7eb77def6a504e0d9ba6a59
nmittu@digite.com <nmittu@digite.com@9b834a4c-f555-3c93-554b-b0e9f3194743>
2014-02-28 05:27:11 +0000

    Changing s-k to sk in the linkedin hover field on our social buttons

--- trunk/templates/shaper_simplicity_ii/js/custom.js
+++ trunk/templates/shaper_simplicity_ii/js/custom.js
@@ -1,163 +1,46 @@
-/*!
- * jQuery Cookie Plugin v1.4.0
- * https://github.com/carhartl/jquery-cookie
- *
- * Copyright 2013 Klaus Hartl
- * Released under the MIT license
- */
-(function (factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as anonymous module.
-        define(['jquery'], factory);
-    } else {
-        // Browser globals.
-        factory(jQuery);
-    }
-}(function ($) {
 
-    var pluses = /\+/g;
-
-    function encode(s) {
-        return config.raw ? s : encodeURIComponent(s);
-    }
-
-    function decode(s) {
-        return config.raw ? s : decodeURIComponent(s);
-    }
-
-    function stringifyCookieValue(value) {
-        return encode(config.json ? JSON.stringify(value) : String(value));
-    }
-
-    function parseCookieValue(s) {
-        if (s.indexOf('"') === 0) {
-            // This is a quoted cookie as according to RFC2068, unescape...
-            s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
-        }
-
-        try {
-            // Replace server-side written pluses with spaces.
-            // If we can't decode the cookie, ignore it, it's unusable.
-            s = decodeURIComponent(s.replace(pluses, ' '));
-        } catch(e) {
-            return;
-        }
-
-        try {
-            // If we can't parse the cookie, ignore it, it's unusable.
-            return config.json ? JSON.parse(s) : s;
-        } catch(e) {}
-    }
-
-    function read(s, converter) {
-        var value = config.raw ? s : parseCookieValue(s);
-        return $.isFunction(converter) ? converter(value) : value;
-    }
-
-    var config = $.cookie = function (key, value, options) {
-
-        // Write
-        if (value !== undefined && !$.isFunction(value)) {
-            options = $.extend({}, config.defaults, options);
-
-            if (typeof options.expires === 'number') {
-                var days = options.expires, t = options.expires = new Date();
-                t.setDate(t.getDate() + days);
-            }
-
-            return (document.cookie = [
-                encode(key), '=', stringifyCookieValue(value),
-                options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-                options.path    ? '; path=' + options.path : '',
-                options.domain  ? '; domain=' + options.domain : '',
-                options.secure  ? '; secure' : ''
-            ].join(''));
-        }
-
-        // Read
-
-        var result = key ? undefined : {};
-
-        // To prevent the for loop in the first place assign an empty array
-        // in case there are no cookies at all. Also prevents odd result when
-        // calling $.cookie().
-        var cookies = document.cookie ? document.cookie.split('; ') : [];
-
-        for (var i = 0, l = cookies.length; i < l; i++) {
-            var parts = cookies[i].split('=');
-            var name = decode(parts.shift());
-            var cookie = parts.join('=');
-
-            if (key && key === name) {
-                // If second argument (value) is a function it's a converter...
-                result = read(cookie, value);
-                break;
-            }
-
-            // Prevent storing a cookie that we couldn't decode.
-            if (!key && (cookie = read(cookie)) !== undefined) {
-                result[name] = cookie;
-            }
-        }
-
-        return result;
-    };
-
-    config.defaults = {};
-
-    $.removeCookie = function (key, options) {
-        if ($.cookie(key) !== undefined) {
-            // Must not alter options, thus extending a fresh object...
-            $.cookie(key, '', $.extend({}, options, { expires: -1 }));
-            return true;
-        }
-        return false;
-    };
-
-}));
-/*JQuery cookie plugin ends here*/
-
+//jQuery.noConflict();
 var social_titles = function(){
     document.getElementById('0').title='Like us on Facebook!';
     document.getElementById('1').title='Talk to us on Twitter!';
-    document.getElementById('2').title='Check out our Blog and give us feedback!';
+    document.getElementById('2').title='Check out our Blog - SwiftStream and give us feedback!';
     document.getElementById('3').title='Check out our video tutorials and webinar recordings!';
-    document.getElementById('4').title='Join the Swift-Kanban User Group on LinkedIn!';
-}
+    document.getElementById('4').title='Join the SwiftKanban User Group on LinkedIn!';
+};
 
-$(function() {
+jQuery(function() {
 if(document.getElementById('partner'))
 {
-    var $tabs = $('#tabs').tabs();
+    var jQuerytabs = jQuery('#tabs').tabs();
     var nav_string;
-    $(".partner-offer").each(function(i){
+    jQuery(".partner-offer").each(function(i){
 
 
-        var totalSize = $(".partner-offer").size() - 1;
+        var totalSize = jQuery(".partner-offer").size() - 1;
         nav_string="<div class='partner-offer-footer'>";
 
         if (i != 0) {
             prev = i;
             nav_string=nav_string+"<a href='#' class='prev-tab mover' rel='" + prev + "'>" +
-                "<img src='http://www.swift-kanban.com/images/partner-prev-btn.png' alt=''></a>";
+                "<img src='http://www.swiftkanban.com/images/partner-prev-btn.png' alt=''></a>";
         }
         if (i != totalSize) {
             next = i + 2;
             nav_string=nav_string+"<a href='#' class='next-tab mover' rel='" + next + "'>" +
-                "<img src='http://www.swift-kanban.com/images/partner-next-btn.png' alt=''></a>";
+                "<img src='http://www.swiftkanban.com/images/partner-next-btn.png' alt=''></a>";
         }
         nav_string=nav_string+"</div>";
 
 
-        $(this).prepend(nav_string);
+        jQuery(this).prepend(nav_string);
 
     });
 
-    $('.next-tab, .prev-tab').click(function() {
-        $tabs.tabs('select', $(this).attr("rel"));
+    jQuery('.next-tab, .prev-tab').click(function() {
+        jQuerytabs.tabs('select', jQuery(this).attr("rel"));
         return false;
     });
-    $( "#tabs" ).bind( "tabsselect", function(event, ui) {
+    jQuery( "#tabs" ).bind( "tabsselect", function(event, ui) {
         document.getElementById('displayme').scrollIntoView();
     });
 
@@ -171,17 +54,17 @@ var display_survey_next_delay = 600;  //time in seconds
 var currentTime = (new Date()).getTime();
 
 if(window.location.pathname.indexOf('free-kanban-trial')>=0)
-    $.cookie("free_trial", true);
+    jQuery.cookie("free_trial", true);
 
-$.cookie("first_visit_time", currentTime); // Sample 2
-if( $.cookie("first_visit_time")==null && $.cookie("survey_popup_displayed")==null)
-    $.cookie("first_visit_time", currentTime);
+jQuery.cookie("first_visit_time", currentTime); // Sample 2
+if( jQuery.cookie("first_visit_time")==null && jQuery.cookie("survey_popup_displayed")==null)
+    jQuery.cookie("first_visit_time", currentTime);
 
-$.doTimeout(1000, function(){
+jQuery.doTimeout(1000, function(){
     var newTime = (new Date()).getTime();
-    if($.cookie("survey_popup_displayed")!=null)
+    if(jQuery.cookie("survey_popup_displayed")!=null)
         return false;
-    if(newTime-$.cookie("first_visit_time") > (display_survey_first_time*1000) )
+    if(newTime-jQuery.cookie("first_visit_time") > (display_survey_first_time*1000) )
     {
         showDialog();
         return false;
@@ -190,21 +73,21 @@ $.doTimeout(1000, function(){
 });
 
 var survey_click = function(btn_value){
-    var survey = $('#survey');
+    var survey = jQuery('#survey');
     survey.dialog('close');
     if(btn_value=='yes')
-        window.open('http://www.swift-kanban.com/survey');
+        window.open('http://www.swiftkanban.com/survey');
 }
 var showDialog = function (popup_type)
 {
-    $.cookie("survey_popup_displayed","true",{ expires: 30 });
+    jQuery.cookie("survey_popup_displayed","true",{ expires: 30 });
     loc = window.location;
     if (loc.pathname.indexOf("free-kanban-trial") >= 0)
         return;
 
-    $.doTimeout(5000, function(){
+    jQuery.doTimeout(5000, function(){
         var popup_type = randomPopupGenerator();
-        if(popup_type==2 && ($.cookie("free_trial")==null))
+        if(popup_type==2 && (jQuery.cookie("free_trial")==null))
             show_free_trial_dialog();
         else
             show_survey_dialog();
@@ -214,7 +97,7 @@ var showDialog = function (popup_type)
 
 var show_survey_dialog = function()
 {
-    $( "#survey" ).dialog({
+    jQuery( "#survey" ).dialog({
 
         width: '457px',
         height: 'auto',
@@ -231,7 +114,7 @@ var show_survey_dialog = function()
 var release_dialog = 0;
 var show_release_dialog = function()
 {
-    $( "#release_30" ).dialog({
+    jQuery( "#release_30" ).dialog({
 
         width: '1000px',
         height: '500px',
@@ -244,22 +127,22 @@ var show_release_dialog = function()
         },
         position: { my: "center", at: "bottom", of: '#popup_head' },
         create: function(event, ui) {
-            //$("body").css({ overflow: 'hidden' })
+            //jQuery("body").css({ overflow: 'hidden' })
             release_dialog=1;
         },
         beforeClose: function(event, ui) {
-            //$("body").css({ overflow: 'inherit' })
+            //jQuery("body").css({ overflow: 'inherit' })
             release_dialog=0;
         }
 
 });
 
-    $.cookie("release_30","true",{ expires: 30 });
+    jQuery.cookie("release_30","true",{ expires: 30 });
 };
 
 var show_free_trial_dialog = function()
 {
-    $( "#survey-get-free" ).dialog({
+    jQuery( "#survey-get-free" ).dialog({
         width: '457px',
         height: 'auto',
         modal: true,
@@ -267,14 +150,14 @@ var show_free_trial_dialog = function()
         draggable: false,
         dialogClass:'free_trial_dialog',
         open: function(event, ui) {
-            $('.free_trial_dialog').click(function() {
-                window.location='http://www.swift-kanban.com/trial-popup';
+            jQuery('.free_trial_dialog').click(function() {
+                window.location='http://www.swiftkanban.com/trial-popup';
             });
 
-            var free_trial_close = $('.ui-icon-closethick');
+            var free_trial_close = jQuery('.ui-icon-closethick');
             free_trial_close.click(function()
             {
-                $('#survey-get-free').dialog('close');
+                jQuery('#survey-get-free').dialog('close');
                 return false;
             });
 
@@ -324,7 +207,7 @@ window.onload = (typeof window.onload != 'function') ? _loadUserVoice : function
 /* javascript for follow twitter popup starts here */
 
 function follow_twitter() {
-    window.open("https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fwww.swift-kanban.com%2Ftest%2Ftest-font-data-uri.html&region=follow_link&screen_name=swiftkanban&tw_p=followbutton&variant=2.0", "mywindow", "menubar=1,resizable=0,width=565,height=520");
+    window.open("https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fwww.swiftkanban.com%2Ftest%2Ftest-font-data-uri.html&region=follow_link&screen_name=swiftkanban&tw_p=followbutton&variant=2.0", "mywindow", "menubar=1,resizable=0,width=565,height=520");
 }
 
 /* javascript for follow twitter popup ends here */
